f6ce297fb3dbe1d2026bf6fb9767fa3ee59e0695
PopovMaja <167343423+PopovMaja@users.noreply.github.com>
2024-05-04 16:34:00 -0400

    function name changed
    function updateTemperature changed to updateForecastInfo

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-function updateTemperature(response) {
+function updateForecastInfo(response) {
   let newTemperature = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let searchedCity = document.querySelector("#city-name");
@@ -20,7 +20,7 @@ function updateTemperature(response) {
 function searchCity(city) {
   let apiKey = "594t1344a33ebb79a8c028f85db36co0";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
-  axios.get(apiUrl).then(updateTemperature);
+  axios.get(apiUrl).then(updateForecastInfo);
 }
 
 function searchResault(event) {
