4126d67dfa9af84646b598a60e23ca0bf4b9f041
PopovMaja <167343423+PopovMaja@users.noreply.github.com>
2024-05-05 21:49:54 -0400

    added time/date info and icon using API

--- src/index.js
+++ src/index.js
@@ -8,7 +8,13 @@ function updateForecastInfo(response) {
   let mph = response.data.wind.speed;
   let realFeel = document.querySelector("#real-feel");
   let feelsLike = response.data.temperature.feels_like;
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
 
+  console.log(response);
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="icon"/>`;
+  timeElement.innerHTML = formatDate(date);
   realFeel.innerHTML = Math.round(feelsLike);
   windSpeed.innerHTML = Math.round(mph);
   humidityElement.innerHTML = response.data.temperature.humidity;
@@ -17,9 +23,51 @@ function updateForecastInfo(response) {
   newTemperature.innerHTML = Math.round(temperature);
 }
 
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+
+  let dateNumber = date.getDate();
+  let year = date.getFullYear();
+  let months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
+  let month = months[date.getMonth()];
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${hours}:${minutes}<br>${month}/${dateNumber}/${year}<br>${day}`;
+}
+
 function searchCity(city) {
   let apiKey = "594t1344a33ebb79a8c028f85db36co0";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(updateForecastInfo);
 }
 
